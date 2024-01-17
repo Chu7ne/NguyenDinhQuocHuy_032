d69ef5bdb2e63857177805d251cd0f8dffeb9812
ADMIN <ADMIN@PC325>
2024-01-17 10:29:26 +0700

    Add project files.

---
+++ BaiKT/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1
+{
+    class Nhanvien
+    {
+        public string HoTen { get; set; }
+        public int SoNgayCong { get; set; }
+        public int SoNgayLamThem { get; set; }
+        public string XepLoai { get; set; }
+        public string BoPhan { get; set; }
+        public Nhanvien() { }
+        public Nhanvien(string hoTen, int soNgayCong, int soNgayLamThem, string xepLoai, string boPhan)
+        {
+            HoTen = hoTen;
+            SoNgayCong = soNgayCong;
+            SoNgayLamThem = soNgayLamThem;
+            XepLoai = xepLoai;
+            BoPhan = boPhan;
+        }
+        public double TinhLuong()
+        {
+            double phanTramTang = (BoPhan == "TrucTiep") ? 3.0 : 2.0;
+            double luongMotNgay = 100000;
+
+            return (SoNgayCong + SoNgayLamThem * (phanTramTang / 100)) * luongMotNgay;
+        }
+        public double TinhThuNhap()
+        {
+            switch (XepLoai)
+            {
+                case "a":
+                    return 1.5 * TinhLuong();
+                case "b":
+                    return 1.2 * TinhLuong();
+                case "c":
+                    return TinhLuong();
+                default:
+                    return 0;
+            }
+        }
+        public void InThongTin()
+        {
+            Console.WriteLine("Ho Ten: ",HoTen);
+            Console.WriteLine("Bo Phan: ",BoPhan);
+            Console.WriteLine("Thu Nhap: {0} VND ",TinhThuNhap());
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nhanvien nv = new Nhanvien();
+            Console.Write("Nhap Ho Ten: ");
+            nv.HoTen = Console.ReadLine();
+            Console.Write("Nhap So Ngay Cong: ");
+            nv.SoNgayCong = int.Parse(Console.ReadLine());
+            Console.Write("Nhap So Ngay Lam Them: ");
+            nv.SoNgayLamThem = int.Parse(Console.ReadLine());
+            Console.Write("Nhap Xep Loai (A/B/C): ");
+            nv.XepLoai = Console.ReadLine();
+            Console.Write("Nhap Bo Phan (TrucTiep/GianTiep): ");
+            nv.BoPhan = Console.ReadLine();
+            nv.InThongTin();
+            Nhanvien nv1 = new Nhanvien("Tran Van Teo", 21, 6, "C", "TrucTiep");
+            nv1.InThongTin();
+        }
+    }
+}
---
+++ BaiTap02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    class ChuNhat
+    {
+        private double Rong, Dai;
+        public ChuNhat(double Rong, double Dai)
+        {
+            this.Rong = Rong;
+            this.Dai = Dai;
+        }
+        public virtual double TinhChuVi()
+        {
+            return 2 * (Rong + Dai);
+        }
+        public virtual double tinhDienTich()
+        {
+            return Rong * Dai;
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Chieu Rong: " + Rong);
+            Console.WriteLine("Chieu Dai: " + Dai);
+            Console.WriteLine("Dien Tich: " + tinhDienTich());
+            Console.WriteLine("Chu vi: " + TinhChuVi());
+        }
+    }
+    class Vuong : ChuNhat
+    {
+        public Vuong(double canh) : base(canh, canh) { }
+        public virtual double tinhcanh()
+        {
+            return TinhChuVi() / 4;
+        }
+        public virtual void Xuat()
+        {
+            Console.WriteLine("Canh hinh vuong" + tinhcanh());
+            Console.WriteLine("Dien tich:" + tinhDienTich());
+            Console.WriteLine("Chu Vi:" + TinhChuVi());
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Nhap chieu dai hinh chu nhat:");
+                double daiCN = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Nhap chieu rong hinh chu nhat");
+                double rongCN = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Nhap canh hinh vuong:");
+                double canhvuong = double.Parse(Console.ReadLine());
+
+                ChuNhat cn = new ChuNhat(daiCN, rongCN);
+                Vuong vuong = new Vuong(canhvuong);
+
+                Console.WriteLine("\n Thong tin Hinh Chu Nhat:");
+                cn.Xuat();
+
+                Console.WriteLine("\n Thong tin hinh vuong:");
+                vuong.Xuat();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
